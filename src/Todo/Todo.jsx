dcd1c885069854ddc94ac6837e95972798ec4fde
GsCorneto <131817217+GsCorneto@users.noreply.github.com>
2023-09-18 19:20:49 -0400

    alterações no css

--- src/Todo/Todo.jsx
+++ src/Todo/Todo.jsx
@@ -10,7 +10,7 @@ export default function ToDo() {
    const salvar = (e) =>{
     e.preventDefault();
     setLista([...lista, { //... serve para adicionar segmentos 
-        nome:nome,inscricao:inscricao
+        nome:nome, inscricao:inscricao, //poder:poder
     }]);
     setIdentidade(inscricao + 1)
     alert("Inscrito no torneio!")
@@ -29,25 +29,35 @@ export default function ToDo() {
 
 
     return (
+        
+        
         <div>
-            <h1>Inscrição do Torneio</h1>
+         <header>
             
-           <form onSubmit={salvar}>
-                <input type="text" onChange={(e)=>{setAtividade(e.target.value)}}></input>
+            <h1>Inscrição do Torneio</h1>
+            </header>
+            <img src="Todo/sword-svgrepo-com.svg"></img>
+            <div class="container">
+            <form onSubmit={salvar}>
+                <p>Nome:</p><input type="text" onChange={(e)=>{setAtividade(e.target.value)}}></input>
                 <br/>
-               
-                {/* <input type="text" onChange={(a)=>{setAtividade(a.target.value)}}></input>  */}
+                 <p>Poder:</p><input type="text" onChange={(a)=>{setAtividade(a.target.value)}}></input>
              <button>Adicionar</button>
            </form>
+            </div>
+           
            <Link to="/">Lobby</Link>
 
            <br/>
            {lista.map((ativ)=>
              <div key={ativ.inscricao}>
-                <p>{ativ.nome}</p>
-                <button onClick={() => remover(ativ.inscricao)}>Desclassificar</button>
+                <h2>Nome:{ativ.nome}</h2>
+                <h2>Poder do combatente{ativ.poder}</h2>
+                <button class="botao" onClick={() => remover(ativ.inscricao)}>Desclassificar</button>
              </div>
            )}
+
+           <footer>Boa Luta!</footer>
         </div>
     );
 }
